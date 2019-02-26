6dae769f632804d8b5c51f4ed91118ccd1cd67d7
a1852rw <a1852rw@aiit.ac.jp>
2019-02-26 21:39:53 +0900

    はじめの一歩を書き込んだ

---
+++ 001_hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+s1 = "hello world !"
+
+print(s1)
---
+++ 002_time.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- codint: utf-8 -*-
+
+import datetime
+
+d1 = datetime.datetime.today()
+print("today: " ,d1)
+
+d2 = datetime.datetime.now()
+print("now: " ,d2)
+
+
+
+
