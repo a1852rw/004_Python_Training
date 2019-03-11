8e4a29157939f7d1cbf5bcdff3718ab57e1d35f6
a1852rw <a1852rw@aiit.ac.jp>
2019-03-11 22:18:46 +0900

    時刻表示のテストおよび天気取得の更新

--- 003_weather_forecast.py
+++ 003_weather_forecast.py
@@ -8,14 +8,14 @@ URL_BASE = "http://api.openweathermap.org/data/2.5/forecast?q="
 URL_CITY = "Tokyo"
 KEY = "d171c64bee9ad5febbba628f6e1b46b7"
 
-print(URL_BASE + URL_CITY + "&appid=" + KEY)
+# print(URL_BASE + URL_CITY + "&appid=" + KEY)
 
 response = requests.get(URL_BASE + URL_CITY + "&appid=" + KEY).json()
-jsontext = json.dumps(response, indent=2)
+jsontext = json.dumps(response, indent=4)
 
 print(jsontext)
 
 data = json.loads(jsontext)
 
-print("現在の風速：" + str(data["wind"]["speed"]) + "m")
+# print("3時間ごとの風速：" + data(["wind"]["speed"]) + "m/s")
 
---
+++ 005_time.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import datetime
+
+t1 = datetime.date.today()
+
+print(t1)
+
+t2 = datetime.datetime.now()
+print(t2)
+
