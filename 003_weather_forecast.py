60a049e6899d4dc655b9d4db426f2c32939f97ae
a1852rw <a1852rw@aiit.ac.jp>
2019-03-11 22:33:44 +0900

    プログラムを若干変更 まだできていない

--- 003_weather_forecast.py
+++ 003_weather_forecast.py
@@ -13,9 +13,11 @@ KEY = "d171c64bee9ad5febbba628f6e1b46b7"
 response = requests.get(URL_BASE + URL_CITY + "&appid=" + KEY).json()
 jsontext = json.dumps(response, indent=4)
 
-print(jsontext)
+# print(jsontext)
 
 data = json.loads(jsontext)
 
 # print("3時間ごとの風速：" + data(["wind"]["speed"]) + "m/s")
+print(data(["cod"]))
+
 
